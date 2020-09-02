881c3b805478169fa323b00ad39f98154f3833b3
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-09-02 11:28:54 +0530

    02/09/20

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char a[6] = "world";
+
+    int i, j;
+
+    for (i = 0, j = 5; i < j; a[i++] = a[j--])
+        ;
+
+    printf("%s\n", a);
+}
